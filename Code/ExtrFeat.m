d7923e0c23ffb37e7b2a35ee527668480b79fa80
Xingyang Cai <tifosi.cai@gmail.com>
2014-03-10 02:07:04 +0800

    read data and load data

--- Code/ExtrFeat.m
+++ Code/ExtrFeat.m
@@ -4,6 +4,30 @@ close all;
 
 %% load data
 
+% datapath = '../dataset/MSRAction3DSkeleton(20joints)/';
+% joint_data = cell(20,10,10);
+% for a = 1:20
+%     for s = 1:10
+%         for e = 1:3
+%             datafn = sprintf('a%02d_s%02d_e%02d_skeleton.txt', a, s, e);
+%             fid = fopen([datapath datafn], 'r');
+%             dataIn = [];
+%             if fid > 0
+%                 dataIn = fscanf(fid, '%f'); 
+%                 dataIn = reshape(dataIn, [4, size(dataIn,1)/4]);
+%                 dataIn = dataIn';
+%                 dataIn = reshape(dataIn, [20, size(dataIn,1)/20, 4]); % dataIn (jointID, FrameID, xyzID)
+%                 fclose(fid);
+%             end
+%             joint_data{a,s,e} = dataIn;
+%         end
+%     end
+% end
+% save '../result/joint_data.mat' joint_data;
+
+load ../result/joint_data.mat;
+
+%% norm
 
 
 %% extr area
