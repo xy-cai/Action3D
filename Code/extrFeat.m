cfa32d33fc8cef4765209842379b0be612a26ab3
Xingyang Cai <tifosi.cai@gmail.com>
2014-03-10 17:32:00 +0800

    extr feat until 17:32

--- Code/extrFeat.m
+++ Code/extrFeat.m
@@ -1,3 +1,5 @@
+function extrFeat
+
 clc;
 clear all;
 close all;
@@ -30,10 +32,13 @@ J=[20     3     3     1     8     10    2     9    11   3   4    7     7     5  
 
 load ../result/joint_data.mat;
 
+%% norm & revise
+
+
 %% visualization
 
 % for a = 1:20
-%     for s = 1:10
+%     for s = 2:2
 %         for e = 1:3
 %             dataIn = joint_data{a,s,e};
 %             if size(dataIn,1)~=0
@@ -47,37 +52,166 @@ load ../result/joint_data.mat;
 %                     set(gca,'DataAspectRatio',[1 1 1])
 %                     axis([70 220 100 250 175 325]);
 %                     hold on;
-%                     for j = 1:19
+%                     for j = 1:size(J,2)
 %                         lineP = [x(J(1,j),f),y(J(1,j),f),z(J(1,j),f);
 %                               x(J(2,j),f),y(J(2,j),f),z(J(2,j),f)]';
 %                         line(lineP(1,:),lineP(2,:),lineP(3,:),'LineWidth',2);
 %                     end
 %                     hold off;
 %                     pause(1/20);
+% %                     pause;
 %                 end
 %             end
 %         end
 %     end
 % end
 
-%% norm
+%% extr area 19*3
 
+directed_area = cell(20,10,3);
+% directed_area{a,s,e}{f,j} action a, subject s, environment e, frame f,
+% limb j; f start from 2
 
-
-%% extr area
-
+for a = 1:20
+    for s = 1:10
+        for e = 1:3
+            dataIn = joint_data{a,s,e};
+            directed_area_one_video = cell(size(dataIn,2),size(J,2));
+            for f = 2:size(dataIn,2)
+                for j = 1:size(J,2)
+                    p1 = squeeze(dataIn(J(1,j),f,:));
+                    p2 = squeeze(dataIn(J(2,j),f,:));
+                    last_p1 = squeeze(dataIn(J(1,j),f-1,:));
+                    last_p2 = squeeze(dataIn(J(2,j),f-1,:));
+                    cur_limb = p2(1:3)-p1(1:3);
+                    last_limb = last_p2(1:3)-last_p1(1:3);
+                    norm_vec = cross(last_limb, cur_limb);
+                    directed_area_one_video{f,j} = [norm_vec; (p1(4)+p2(4)+last_p1(4)+last_p2(4))/4];
+                end
+            end
+            directed_area{a,s,e} = directed_area_one_video;
+        end
+    end
+end
 
 %% relative position (20-1)*3 = 57 [ICCV13 GSGC-DL]
 
+% relative_position{a,s,e}{f,j} action a, subject s, environment e, frame f,
+% limb j
 
+relative_pos_57 = cell(20,10,3);
+for a = 1:20
+    for s = 1:10
+        for e = 1:3
+            dataIn = joint_data{a,s,e};
+            relative_pos_57_one_video = cell(size(dataIn,2),size(J,2));
+            for f = 1:size(dataIn,2)
+                for j = 1:size(J,2)
+                    p1 = squeeze(dataIn(J(1,j),f,:));
+                    p2 = squeeze(dataIn(J(2,j),f,:));
+                    relative_pos_57_one_video{f,j} = [p2(1:3)-p1(1:3), (p2(4)+p1(4))/2];
+                end
+            end
+            relative_pos_57{a,s,e} = relative_pos_57_one_video;
+        end
+    end
+end
 
 %% motion vector 20*20 [IJCV 13 Accuracy and Observational]
 
-%% relations of every paired joint 20*19 [IJCV 13 Accuracy and Observational]
+% motion_vector_20_20{a,s,e}{f} (400+1)x1 action a, subject s, enviroment e, frame
+% f; f start from 2
+
+motion_vector_20_20 = cell(20,10,3)
+
+for a = 1:20
+    for s = 1:10
+        for e = 1:3
+            dataIn = joint_data{a,s,e};
+            motion_vector_20_20_one_video = cell(size(dataIn,2));
+            for f = 2:size(dataIn,2)
+                motion_vector_20_20_one_video{f} = zeros(size(dataIn,1)*size(dataIn,1)+1,1);
+                sum_prob = 0;
+                for i = 1:size(dataIn,1)
+                    for j = 1:size(dataIn,1)
+                        p1 = squeeze(dataIn(i,f,:));
+                        p2 = squeeze(dataIn(j,f-1,:));
+                        motion_vector_20_20_one_video{f}((i-1)*size(dataIn,1)+j) = sqrt(sum((p1(1:3)-p2(1:3)).^2));
+                        sum_prob = sum_prob + p1(4)+p2(4);
+                    end
+                end
+                motion_vector_20_20_one_video{f}(size(dataIn,1)*size(dataIn,1)+1) = sum_porb/(2*size(dataIn,1)*size(dataIn,1));
+                
+            end
+            motion_vector_20_20{a,s,e} = motion_vector_20_20_one_video;
+        end
+    end
+end
+
+%% relations of every paired joint 20*19/2 [IJCV 13 Accuracy and Observational]
+
+% relations_20_19{a,s,e}{f} action a subject s enviroment e frame f 190x1
+
+relations_20_19 = cell(20,10,3);
+
+for a = 1:20
+    for s = 1:10
+        for e = 1:3
+            dataIn = joint_data{a,s,e};
+            relations_20_19_one_video = cell{size(dataIn,2)};
+            for f = 1:size(dataIn,2)
+                relations_20_19_one_video{f} = zeros(size(dataIn,1)*(size(dataIn,1)-1)/2+1,1);
+                sum_prob = 0;
+                cnt = 1;
+                for i = 1:size(dataIn,1)
+                    for j = i+1:size(dataIn,1)
+                        p1 = squeeze(dataIn(i,f,:));
+                        p2 = squeeze(dataIn(j,f,:));
+                        relations_20_19_one_video{f}(cnt) = sqrt(sum((p1(1:3)-p2(1:3)).^2));
+                        cnt = cnt+1;
+                        sum_prob = sum_prob+p1(4)+p2(4);
+                    end
+                end 
+                relations_20_19_one_video{f}(cnt) = sum_prob/(2*(cnt-1));
+            end
+            relations_20_19{a,s,e} = relations_20_19_one_video;
+        end
+    end
+end
 
 %% Dist paired joints norm by path [MM13]
 
 %% Moving Pose [ICCV 13]
+
+% moving_pose{a,s,e}{f} f start from 3, end to size(dataIn,2)-2
+
+moving_pose = cell(20,10,3);
+alpha = 0.75;
+beta = 0.6;
+
+for a = 1:20
+    for s = 1:10
+        for e = 1:3
+            dataIn = joint_data{a,s,e};
+            moving_pose_one_video = cell{size(dataIn,2)};
+            for f = 3:size(dataIn,2)-2
+                P2 = zeros(size(dataIn,1)*3,1);
+                P1 = zeros(size(dataIn,1)*3,1);
+                P0 = zeros(size(dataIn,1)*3,1);
+                Pm1 = zeros(size(dataIn,1)*3,1);
+                Pm2 = zeros(size(dataIn,1)*3,1);
+                for i = 1:size(dataIn,1)
+                    P2((i-1)*3+1:i*3) = dataIn(i, f+2, 1:3);
+                    P1((i-1)*3+1:i*3) = dataIn(i, f+1, 1:3);
+                    P0((i-1)*3+1:i*3) = dataIn(i, f, 1:3);
+                    Pm1((i-1)*3+1:i*3) = dataIn(i, f-1, 1:3);
+                    Pm2((i-1)*3+1:i*3) = dataIn(i, f-2, 1:3);
+                end
+                moving_pose_one_video{f} = [P0, alpha*(P1-Pm1), beta*(P2+Pm2-2*P0)];
+            end
+        end
+    end
+end
 
 %% motion vector (20-1)*3 [...]
  
@@ -86,3 +220,10 @@ load ../result/joint_data.mat;
 %% JMO
 
 %% 
+end
+
+function [a,b] = swap(a,b)
+    tmp = a;
+    a = b;
+    b = tmp;
+end
